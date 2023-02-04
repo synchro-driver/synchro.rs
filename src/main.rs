e58b7f87b2ea5f5ec47f25b6cd8cb12484f05340
CyberFlaw <kannan.pappus@gmail.com>
2023-02-04 21:01:51 +0530

    ready for development

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
 // extern crate protocol;
 // extern crate server;
 // extern crate client;
-
-use rodio::source::{SineWave, Source};
-use rodio::{OutputStream, Sink};
-use std::time::Duration;
+// extern crate middleware;
 
 fn main() {
-    // println!("{}", protocol::add(5, 6));
-
-    let output_streams = OutputStream::try_default();
-    let stream_handler = match output_streams {
-        Ok((_, strean_handler)) => strean_handler,
-
-        Err(_) => panic!("No inputs found!"),
-    };
-
-    let sink = Sink::try_new(&stream_handler).unwrap();
-
-    let source = SineWave::new(440.0)
-        .take_duration(Duration::from_secs_f32(2.0))
-        .amplify(0.5);
-
-    sink.append(source);
-    sink.sleep_until_end();
+    // middleware::test::gen_sin(2.0, 440.0);
 }
