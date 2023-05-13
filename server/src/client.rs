5b6467943c8dbc330705d22daa111bfc7bf1c407
ByteCrak07 <abilsavio@gmail.com>
2023-05-14 03:07:30 +0530

    Simple packet sending server complete

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,51 +1,34 @@
-use std::io::{BufRead, BufReader, Write};
+use serde::{Deserialize, Serialize};
+use std::io::{BufRead, BufReader};
 use std::net::TcpStream;
 
-use serde::{Deserialize, Serialize};
-
 #[derive(Serialize, Deserialize)]
-struct Message {
-    sender: String,
+struct Packet {
     content: String,
+    timestamp: i64,
 }
 
 pub fn start() {
-    let mut stream = TcpStream::connect("127.0.0.1:8000").unwrap();
+    let stream = TcpStream::connect("127.0.0.1:8000").unwrap();
     println!("Connected to server: {}", stream.peer_addr().unwrap());
 
     loop {
-        print!("Enter a message: ");
-        std::io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        // Serialize the input message
-        let message = Message {
-            sender: "Client".to_string(),
-            content: input.trim().to_string(),
-        };
-        let serialized_message = serde_json::to_string(&message).unwrap();
-
-        // Send the serialized message to the server
-        stream.write_all(serialized_message.as_bytes()).unwrap();
-
-        // Receive the response from the server
+        // Receive the response from broadcaster
         let mut reader = BufReader::new(&stream);
         let mut buffer = String::new();
         let bytes_read = reader.read_line(&mut buffer).unwrap();
 
         if bytes_read == 0 {
             // Connection closed
-            println!("Connection closed by server");
+            println!("Connection closed by host");
             break;
         }
 
-        // Deserialize the response message
-        let response: Message = serde_json::from_str(&buffer).unwrap();
+        // Deserialize the packet message
+        let packet: Packet = serde_json::from_str(&buffer).unwrap();
         println!(
-            "Received response from {}: {}",
-            response.sender, response.content
+            "Packet {} from server at timestamp: {}",
+            packet.content, packet.timestamp
         );
     }
 }
--- server/src/host.rs
+++ server/src/host.rs
@@ -1,107 +1,21 @@
-use serde::{Deserialize, Serialize};
-// use std::io::{BufRead, BufReader, Write};
-// use std::net::{TcpListener, TcpStream};
-// use std::thread;
-use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
-};
+use std::{io::Write, net::TcpStream, thread, time::Duration};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Message {
-    sender: String,
-    content: String,
-}
+pub fn start() {
+    let mut stream = TcpStream::connect("127.0.0.1:8000").unwrap();
+    println!("Sending packets");
 
-// fn handle_client(stream: TcpStream) {
-//     let mut stream_clone = stream.try_clone().expect("clone failed...");
-
-//     let mut reader = BufReader::new(&stream);
-//     let mut buffer = String::new();
-
-//     loop {
-//         buffer.clear();
-//         let bytes_read = reader.read_line(&mut buffer).unwrap();
-
-//         if bytes_read == 0 {
-//             // Connection closed
-//             println!("Connection closed: {}", stream.peer_addr().unwrap());
-//             break;
-//         }
-
-//         // Deserialize the received message
-//         let message: Message = serde_json::from_str(&buffer).unwrap();
-//         println!(
-//             "Received message from {}: {}",
-//             message.sender, message.content
-//         );
-
-//         // Echo back the received message
-//         let response = Message {
-//             sender: "Server".to_string(),
-//             content: message.content,
-//         };
-//         let serialized_response = serde_json::to_string(&response).unwrap();
-//         stream_clone
-//             .write_all(serialized_response.as_bytes())
-//             .unwrap();
-//     }
-// }
-
-// pub fn start() {
-//     const PORT: i32 = 8000;
-
-//     println!("Starting as a host at port {}", PORT);
-
-//     let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT)).unwrap();
-
-//     // Handle incoming connections
-//     for stream in listener.incoming() {
-//         match stream {
-//             Ok(stream) => {
-//                 println!("New connection: {}", stream.peer_addr().unwrap());
-//                 // Spawn a new thread for each incoming connection
-//                 thread::spawn(move || {
-//                     handle_client(stream);
-//                 });
-//             }
-//             Err(e) => {
-//                 println!("Error: {}", e);
-//             }
-//         }
-//     }
-// }
-
-#[tokio::main]
-pub async fn start() {
-    const PORT: i32 = 8000;
-
-    println!("Starting as a host at port {}", PORT);
-
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT))
-        .await
-        .unwrap();
+    let mut packet_no = 0;
 
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        // send dummy message to the broadcaster
+        stream
+            .write_all(format!("{}\n", packet_no).as_bytes())
+            .unwrap();
 
-        tokio::spawn(async move {
-            let (read, mut write) = socket.split();
+        // Pause for 100ms
+        thread::sleep(Duration::from_millis(100));
 
-            let mut reader = BufReader::new(read);
-
-            let mut line = String::new();
-
-            loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-
-                if bytes_read == 0 {
-                    break;
-                }
-
-                write.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
-            }
-        });
+        // increment packet count
+        packet_no += 1;
     }
 }
