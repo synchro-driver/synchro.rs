afc3b7d5fdfcab07b584edf29b7c0e93e16f54e3
CyberFlaw <kannan.pappus@gmail.com>
2023-05-15 00:49:32 +0530

    added MessagePack to serde

--- protocol/src/raw.rs
+++ protocol/src/raw.rs
@@ -1,5 +1,4 @@
-use std::marker::PhantomData;
-
+use rmp_serde;
 use serde::{Deserialize, Serialize};
 
 // Set Format as pcm::Format::S16LE in reciver side
@@ -24,7 +23,8 @@ pub struct Stream {
     pub buffer_size: u16,
 
     // pub stream: &'a mut [f64],
-    pub stream: PhantomData<[f64]>,
+    // pub stream: [f64; 1024],
+    pub stream: Vec<f64>,
 
     // used to check the order in which the packets arrive
     // similar to sliding window, use it with mod(size(u32)) to prevent overflows
@@ -32,6 +32,88 @@ pub struct Stream {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct StreamInitiation {
-    pub fin: bool,
+pub struct StreamControl {
+    pub alive: bool,
+}
+
+// TODO: Handle deserialize code inside impl blocks
+
+impl Handshake<'_> {
+    pub fn new(
+        buffer_size: u16,
+        source: &str,
+        rate: u32,
+        channels: u32,
+        timestamp: u64,
+    ) -> Handshake {
+        Handshake {
+            buffer_size,
+            source,
+            rate,
+            channels,
+            timestamp,
+        }
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        match rmp_serde::to_vec(&self) {
+            Ok(val) => val,
+            Err(_) => {
+                println!("serialization failed");
+                Vec::new()
+            }
+        }
+    }
+}
+
+impl HandshakeResponce {
+    pub fn new(latency: usize) -> Self {
+        HandshakeResponce { latency }
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        match rmp_serde::to_vec(&self) {
+            Ok(val) => val,
+            Err(_) => {
+                println!("serialization failed");
+                Vec::new()
+            }
+        }
+    }
+}
+
+impl Stream {
+    pub fn new(buffer_size: u16, packet_flag: u32, stream: &[f64]) -> Self {
+        Stream {
+            buffer_size,
+            stream: stream.to_vec(),
+            packet_flag,
+        }
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        match rmp_serde::to_vec(&self) {
+            Ok(val) => val,
+            Err(_) => {
+                println!("serialization failed");
+                Vec::new()
+            }
+        }
+    }
+}
+
+impl StreamControl {
+    pub fn new(state: bool) -> Self {
+        StreamControl { alive: state }
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        match rmp_serde::to_vec(&self) {
+            Ok(val) => val,
+            Err(_) => {
+                println!("serialization failed");
+                Vec::new()
+            }
+        }
+    }
 }
